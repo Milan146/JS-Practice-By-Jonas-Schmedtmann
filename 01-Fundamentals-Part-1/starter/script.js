24dbac1a2a4114f7745f273d0f239fbfcf629c86
Milan146 <81798808+Milan146@users.noreply.github.com>
2022-10-20 16:33:34 +0530

    Learned JS

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,32 @@
 let js = "Amezing";
 if (js === "Amezing") alert("Javascript is fun");
-
 console.log(40 + 8 + 2);
+
+// Variables
+
+let fName = "Lakhani";
+console.log(fName);
+
+// Assignment 1 Values & Varieables
+
+let country = "India";
+let continent = "Asia";
+let population = "1Million";
+
+console.log(
+  "Hi my name is " +
+    fName +
+    ". I am from " +
+    country +
+    " Which comes under " +
+    continent +
+    ", Population over here is : " +
+    population +
+    "!"
+);
+
+let girlFriend = false;
+console.log(girlFriend);
+
+console.log(typeof girlFriend);
+console.log(typeof fName);
